b1460b55b1a7157802b50cb13ff7b554ccdb8db7
shaikhamaan <amaan.shaikh@walchandsangli.ac.in>
2022-05-05 19:05:08 +0530

    updated port in index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Mentorpoint Server is running on port ${PORT}.`);
 });
 
