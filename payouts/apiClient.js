bbd46dcaca78d32a253eeac163359d81c57dbc9f
Dnyaneshwar <dnyaneshwar.ware@walchandsangli.ac.in>
2022-05-09 22:41:30 +0530

    Added cashfree payments payouts APIs

--- payouts/apiClient.js
+++ payouts/apiClient.js
@@ -2,8 +2,8 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL: "https://payout-gamma.cashfree.com/payout/v1",
   headers: {
-    "X-Client-Id": "CF164002C9SJ5FRG1A0QGEN8J9D0",
-    "X-Client-Secret": "48c697fbc97103b7f1bb76f730638c088d9060a1",
+    "X-Client-Id": process.env.CASHFREE_CLIENTID,
+    "X-Client-Secret": process.env.CASHFREE_CLIENTSECRET,
   },
 });
 
